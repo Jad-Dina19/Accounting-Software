1694b021831beae00902cb9ffa19c41e3018c1aa
Jad Dina <jad.dina19@gmail.com>
2024-11-28 00:35:13 -0700

    more edits

--- codingrooms-project/design_project.py
+++ codingrooms-project/design_project.py
@@ -16,14 +16,6 @@ def max(data_list):
     return np.max(max_list)
 
 
-def max(data_list):
-    max_list=[]
-    for row in data_list:
-        for col in row:
-            if type(col) == float:
-                max_list.append(col)
-    return np.max(max_list)
-
 def min(data_list):
     min_list=[]
     for row in data_list:
@@ -51,9 +43,49 @@ def mean(data_list):
                 mean_list.append(col)
     return np.mean(mean_list)
 
+def search_wage(name, lst):
+    for row in lst:
+        for i in range(len(row)):
+            if row[i] == name:
+                return row[2]
+
+def income_tax(wage):
+    
+   #taxes are based of gov alberta tax brackets less that $250000
+    if wage <= 148269:
+                
+        return 0.10 * wage
+            
+    elif 148269.01 <= wage <=177922:
+                
+        return 0.12 * wage
+            
+    elif 177922.01 <= wage <= 237230:
+
+        return 0.13 * wage
+            
+    else:
+
+        return 0.14 * wage
+
+def net_income(income_tax, wage):
+    return wage - income_tax
+
+def store_tax(name, income_tax, net_income, tax_list):
+    count = 0
+    for row in tax_list:
+        for i in range(len(row)):
+            if row[0] == name:
+                row[2] = net_income
+                row[3] = income_tax
+                tax_list[count] = row
+                return tax_list
+        count += 1
+
+
 def menu():
     while(True):    
-        lst = ["Personal Income", "Company Billables", "Company Payments", "Exit"]
+        lst = ["Personal Details", "Company Billables", "Company Payments", "Exit"]
         print("Welcome! Please select an option to view:\n")
         for index, option in enumerate(lst, start=1):
             print(f"\t{index}.  {option}")
@@ -72,7 +104,7 @@ def menu():
 
 def path1():
     while(True):
-        print("What would you like to view?")    
+        print("\nPersonal Details: Select an option:")    
         lst = ["Personal Income", "Average Wage for Position", "Net Income", "Exit"]
 
         for index, option in enumerate(lst, start=1):
@@ -96,18 +128,55 @@ def path3():
     pass
 
 
-while(True):    
-    option = menu()
-    if option == 1:
-        while(True):
-            path1_opt = path1()
-            if path1_opt == 1:
-                name = input("Enter name of employee who's income you would like to view:")
+#Runs the main program
+if __name__ == "__main__":
+    while(True):    
+        option = menu()
+        if option == 1:
 
-    elif option == 2:
-        path2()
-    elif option == 3:
-        path3()
-    else:
-        break
+            while(True):
+
+                path1_opt = path1()
+                if path1_opt == 1:
+                    
+                    name = input("Enter name of employee who's income you would like to view:")
+                    employee_list = csv.read_csv("Employees", False)
+                    wage = search_wage(name, employee_list)
+
+                    print(f"{name}'s wage is ${wage:.2f}.\n")
+
+                elif path1_opt == 2:
+                    posi = input("Enter the position you want to check average wage for Ex(Software Engineer):")
+                    employee_list = csv.read_csv("Employees", False)
+                    average_wage = mean(position(employee_list, posi))
+
+                    print(f"The average wage for the {posi} position is ${average_wage:.2f}.")
+                
+                elif path1_opt == 3:
+                    name = input("Enter name of employee who's income tax you would like to calculate")
+                    employee_list = csv.read_csv("Employees", False)
+                    
+                    wage = search_wage(name, employee_list)
+
+                    tax = income_tax(wage)
+
+                    net = net_income(tax, wage)
+
+                    print(f"{name} had a gross annual salary of {wage:.2f}, an income tax of ${tax:.2f}, and a net income of ${net:.2f}\n")
+
+                    tax_list = csv.read_csv("Taxes")
+                    updated_list = store_tax(name, tax, net, tax_list)
+                    csv.write_csv("Taxes", updated_list, True)
+                    print(f"{name}'s Tax sheet has been updataed\n")
+                else:
+                    break
+                
+                continue
+
+        elif option == 2:
+            path2()
+        elif option == 3:
+            path3()
+        else:
+            break
     
--- codingrooms-project/user_csv.py
+++ codingrooms-project/user_csv.py
@@ -12,7 +12,7 @@ def read_csv(file_name, include_headers = True):
     if(include_headers):
         for line in file:
             data += [line.replace("\n","").split(",")]
-            data[len(data)-1][2] = float(data[len(data)-1][2])
+            
     
     else:
         count = 0
@@ -20,16 +20,50 @@ def read_csv(file_name, include_headers = True):
             
             data += [line.replace("\n","").split(",")]
 
-            if count != 0:
-                data[len(data)-1][2] = float(data[len(data)-1][2])
-            
-            count+= 1
         data.pop(0)
+    
+    new_data = []
+    for row in data:
+        for i in range(len(row)):
+            
+            if row[i].isnumeric():
+                row[i] = float(row[i])    
+        new_data.append(row)
+   
+    return new_data
 
-    return data
 
+def write_csv(file_name, data, overwrite):
+    if overwrite:  
+        file = open(f"data_files/{file_name}.csv", "w")
+        
+        for row in data:
+            
+            for i in range(len(row)):
+            
+                if i == (len(row) -1):
+                
+                    file.write(f"{row[i]}\n")
+                else:
 
-def write_csv():
-    pass
+                    file.write(f"{row[i]},")
+        file.close()
+    
+    else:
+        file = open(f"data_files/{file_name}.csv", "w")
+        count = 0
+        for row in data:
+            
+            for i in range(len(row)):
+                if i == (len(row) -1) and count == len(data) - 1:
+                    
+                    file.write(f"{row[i]}")
+                elif i == (len(row) -1):
+                
+                    file.write(f"{row[i]}\n")
+                else:
 
+                    file.write(f"{row[i]},")
+            count+=1
+        file.close()
 
