b37d73bcf89a9c12f9fceb1be89048c2f5b11849
Jad Dina <jad.dina19@gmail.com>
2024-12-03 20:08:08 -0800

    added 2D numpy arrays

--- user_csv.py
+++ user_csv.py
@@ -25,12 +25,12 @@ def isfloat(num):
 
 def read_csv(file_name, include_headers = True):
     """
-    creates a 2D list of a csv file with or without headers
+    creates a 2D array of a csv file with or without headers
 
     parameters: file_name: string, name of the file being read
                 include_headers(optional): boolean 
 
-    returns: 2D list of data read from csv file
+    returns: 2D array of data read from csv file
     """
 
     file = open(f"data_files/{file_name}.csv", "r") #opens file
@@ -54,28 +54,35 @@ def read_csv(file_name, include_headers = True):
 
         data.pop(0) #removes headers
     
-
-    new_data = [] #new list is created
-
-    #iterates through every index value in the 2D list
+   
+    data = np.array(data)  #new array is created from the list
+    
+    updated_rows = []
+    #iterates through every index value in the 2D array
     for row in data:
+
+        updated_row = []    #holds row updated with floats
+
         for i in range(len(row)):
             
             #if is float is true the value gets casted to a float and put back into the row
             if isfloat(row[i]):
-                row[i] = float(row[i])   
+                updated_row.append(float(row[i])) 
+            else:
+                updated_row.append(row[i]) 
 
-        new_data.append(row) #the updated row is appended to the new list
-   
-    return new_data #the 2D list is returned
+        updated_rows.append(updated_row) #the updated row is appended to the new 2D list
+    
+    
+    return np.array(updated_rows, dtype = object) #the 2D list is returned as a 2D array
 
 
 def write_csv(file_name, data, overwrite):
     """
-    writes a 2D list of data into a csv file
+    writes a 2D array of data into a csv file
 
     parameters: file_name: string name of the csv file
-                data: 2D list of data to be written
+                data: 2D array of data to be written
                 overwrite: boolean 
     """
 
@@ -84,7 +91,7 @@ def write_csv(file_name, data, overwrite):
         
         file = open(f"data_files/{file_name}.csv", "w") #opens file with w which overwrites
         
-        #iterates through the index of each value in the 2D list
+        #iterates through the index of each value in the 2D array
         for row in data:
             
             for i in range(len(row)):
@@ -104,7 +111,7 @@ def write_csv(file_name, data, overwrite):
         
         file = open(f"data_files/{file_name}.csv", "a")
     
-        #iterates through the index of each value in the 2D list
+        #iterates through the index of each value in the 2D array
         for row in data:
             
             for i in range(len(row)): #opens file with a which appends to existing data
